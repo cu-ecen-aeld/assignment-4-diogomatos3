a312187011dd8f3f73c2c0b50962d24a870b552c
Diogo Matos <diogo.matos3@pt.bosch.com>
2024-12-16 11:03:35 +0000

    Update on app finder side

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'daab24c5ca9e4af60b475f491f126aac939d436d'
+AESD_ASSIGNMENTS_VERSION = '9d7b3e66b69aaa9b3a6ea9666ce00c7f2a00ce6b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
