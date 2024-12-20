dd47be963d67378b17059ddae13e1fba3e076b81
Diogo Matos <diogo.matos3@pt.bosch.com>
2024-12-20 17:58:45 +0000

    Debugging

--- clean.sh
+++ clean.sh
@@ -1,11 +1,4 @@
-# Change directory to the directory of the script
-cd `dirname $0`
+#!/bin/sh
+# Cleans Buildroot
 
-# Change directory to 'buildroot'
-cd buildroot
-
-# Clean the build environment
-make distclean
-
-# Change directory back to the directory of the script
-cd `dirname $0`
+make -C buildroot distclean
